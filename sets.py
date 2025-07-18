a4ab5d9c5cdb9afe5daa3fa8c8b94a1c28c7b514
krisharavsj <krisharsv@gmail.com>
2025-07-18 20:30:30 +0530

    set.py

---
+++ sets.py
@@ -0,0 +1,10 @@
+my_set={1,2,3,4,5}
+print(my_set)
+set_2={2.5,"hello",(1,2,3)}
+print(set_2)
+set_3={1,2,3,2,4,3,6,5,4}
+print(set_3)
+set_4=set([1,2,3])
+print(set_4)
+set_4.pop()
+print("after using pop", set_4)
