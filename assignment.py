9d62d04f0d87b4776ed16c77297c45e8d69592d5
krisharavsj <krisharsv@gmail.com>
2025-07-21 19:58:42 +0530

    assignment.py

---
+++ assignment.py
@@ -0,0 +1,6 @@
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+result_method = set1.symmetric_difference(set2)
+print("Symmetric difference using method", result_method)
+result_operator = set1 ^ set2
+print("Symmetric difference using ^ operator", result_operator)
